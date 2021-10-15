f40bc1047e047fe6bad7988137806d7e13d00f9e
Samuel Moncao <samu.henr@gmail.com>
2021-10-15 10:01:26 -0300

    feat: Create style global aplication

---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+export default {
+  colors: {
+    header: '#1B1B1F',
+    
+    background: {
+      primary: '#F4F5F6',
+      secondary: '#FFF',
+    },
+
+    text: {
+      primary: '#7A7A80',
+      detail: '#AEAEB3',
+      title: '#47474D',
+    },
+
+    line: '#EBEBF0',
+    main: {
+      primary: '#DC1637',
+      light: '#FDEDEF',
+    },
+
+    success: '#03B252',
+
+    shape: {
+      primary: '#E1E1E8',
+      dark: '#29292E',
+    },
+  },
+  fonts: {
+    primary: {
+      regular: 'Inter_400Regular',
+      medium: 'Inter_500Medium',
+    },
+    secondary: {
+      regular: 'Archivo_400Regular',
+      medium: 'Archivo_500Medium',
+      semiBold: 'Archivo_600SemiBold',
+    },
+  },
+};
